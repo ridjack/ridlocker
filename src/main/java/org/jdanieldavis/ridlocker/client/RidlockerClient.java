31a6da1938f1101ffefb031521b086c949a4498c
J. Daniel Davis <joshuadlilley@gmail.com>
2024-10-31 11:04:30 -0400

    Initial commit.

---
+++ src/main/java/org/jdanieldavis/ridlocker/client/RidlockerClient.java
@@ -0,0 +1,14 @@
+package org.jdanieldavis.ridlocker.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RidlockerClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+
+}
